addba3ae5b4ab05b0c3b05446ad43ed6a9881eb2
GiSeong Eom <jurist@kldp.org>
2018-12-25 14:18:17 +0900

    Install PSReadLine PS module (below Win10)

--- vmware/scripts/install-ps-modules.ps1
+++ vmware/scripts/install-ps-modules.ps1
@@ -3,3 +3,8 @@
 Install-PackageProvider -Name NuGet -Force -Verbose
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery -Verbose
 Install-module -Name PSWindowsUpdate -Force -Verbose
+
+$BuildVersion = [System.Environment]::OSVersion.Version
+if ($BuildVersion.Major -lt '10') {
+    Install-module -Name PSReadLine -Force -Verbose
+}
