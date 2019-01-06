38d2229d5a7f46eff162e13f0dac62f15dc11092
GiSeong Eom <jurist@kldp.org>
2019-01-06 11:20:02 +0900

    Fixed timing bug

--- vmware/scripts/install-windows-update-on-win10.ps1
+++ vmware/scripts/install-windows-update-on-win10.ps1
@@ -4,24 +4,29 @@
 # https://www.altaro.com/msp-dojo/powershell-windows-updates/
 
 Import-Module PSWindowsUpdate -force
-$updates = Get-wulist -verbose
+$updates = Get-wulist -AutoSelectOnWebSites -Verbose
 
 # cleanup
-if ((get-ScheduledTask -TaskName PSWindowsUpdate -ErrorAction SilentlyContinue | Measure-Object).count -gt 0) { 
-    Remove-Item -Force -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log
+$pswindows_update_task = (get-ScheduledTask -TaskName PSWindowsUpdate -ErrorAction SilentlyContinue)
+if (($pswindows_update_task.TaskName.length -gt 0) -And ($pswindows_update_task.Status -eq 'Ready')) {
     Unregister-ScheduledTask -TaskName PSWindowsUpdate -Confirm:$false
 }
 
 # install
 if ($null -ne $updates) {
-    $WUJob_Script = {import-module PSWindowsUpdate; Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot  | Out-File C:\Windows\Temp\PSWindowsUpdate.log}
+    Remove-Item -Force -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log
+    $WUJob_Script = {import-module PSWindowsUpdate; Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot -AutoSelectOnWebSites | Out-File C:\Windows\Temp\PSWindowsUpdate.log}
     Invoke-WUjob -Confirm:$false -RunNow -Script $WUJob_Script
 
     "Currently processing the following update:"
     Start-Sleep 30
     $log1 = ''
     $log2 = ''
-    While ((Get-ScheduledTask -TaskName pswindowsupdate).state -eq 'Running') {
+    While ($true) {
+        $pswindows_update_task = (get-ScheduledTask -TaskName PSWindowsUpdate -ErrorAction SilentlyContinue)
+        $pswindows_powershell_process = (gcim win32_process -Filter "name = 'powershell.exe'" -ErrorAction SilentlyContinue | Where-Object { $_.CommandLine -like "*PSWindowsUpdate*" } | measure-object)
+        if (($pswindows_update_task.Status -ne 'Running') -And ($pswindows_powershell_process.Count -eq 0)) { Break }
+
         $log1=(Get-Content -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log | select-object -last 1)
         if ($log1 -ne $log2) { $log1 }
               
@@ -31,8 +36,9 @@ if ($null -ne $updates) {
         if ($log2 -ne $log1) { $log2 }
     }
 
-    if ((Get-ScheduledTask -TaskName pswindowsupdate).state -eq 'Ready') {
-        Remove-Item -Force -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log
+
+    $pswindows_update_task = (get-ScheduledTask -TaskName PSWindowsUpdate -ErrorAction SilentlyContinue)
+    if ($pswindows_update_task.Status -eq 'Ready') {
         Unregister-ScheduledTask -TaskName PSWindowsUpdate -Confirm:$false
     }
 }
--- vmware/scripts/install-windows-update-on-win7.ps1
+++ vmware/scripts/install-windows-update-on-win7.ps1
@@ -4,17 +4,17 @@
 # https://www.altaro.com/msp-dojo/powershell-windows-updates/
 
 Import-Module PSWindowsUpdate -force
-$updates = Get-wulist -AutoSelectOnWebSites -verbose
+$updates = Get-wulist -AutoSelectOnWebSites -Verbose
 
 # cleanup
 $pswindows_update_task = (schtasks /query /tn PSWindowsUpdate /FO CSV 2> null | ConvertFrom-Csv)
 if (($pswindows_update_task.TaskName.length -gt 0) -And ($pswindows_update_task.Status -eq 'Ready')) {
-    Remove-Item -Force -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log
     schtasks /Delete /tn PSWindowsUpdate /F
 }
 
 # install
 if ($null -ne $updates) {
+    Remove-Item -Force -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log
     $WUJob_Script = {import-module PSWindowsUpdate; Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot -AutoSelectOnWebSites | Out-File C:\Windows\Temp\PSWindowsUpdate.log}
     Invoke-WUjob -Confirm:$false -RunNow -Script $WUJob_Script
 
@@ -22,19 +22,26 @@ if ($null -ne $updates) {
     Start-Sleep 30
     $log1 = ''
     $log2 = ''
-    While ((schtasks /query /tn PSWindowsUpdate /FO CSV | ConvertFrom-Csv).Status -eq 'Running') {
+    While ($true) {
+        $pswindows_update_task = (schtasks /query /tn PSWindowsUpdate /FO CSV 2> null | ConvertFrom-Csv)
+        $pswindows_powershell_process = (gcim win32_process -Filter "name = 'powershell.exe'" -ErrorAction SilentlyContinue | Where-Object { $_.CommandLine -like "*PSWindowsUpdate*" } | measure-object)
+        if (($pswindows_update_task.Status -ne 'Running') -And ($pswindows_powershell_process.Count -eq 0)) { Break }
+
         $log1=(Get-Content -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log | select-object -last 1)
         if ($log1 -ne $log2) { $log1 }
-              
-        Start-Sleep 10
-        
+
+        Start-Sleep 15
+
         $log2=(Get-Content -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log | select-object -last 1)
         if ($log2 -ne $log1) { $log2 }
     }
 
-    if ((schtasks /query /tn PSWindowsUpdate /FO CSV | ConvertFrom-Csv).Status -eq 'Ready') {
-        Remove-Item -Force -ErrorAction SilentlyContinue C:\Windows\temp\PSWindowsUpdate.log
+
+    $pswindows_update_task = (schtasks /query /tn PSWindowsUpdate /FO CSV 2> null | ConvertFrom-Csv)
+    if ($pswindows_update_task.Status -eq 'Ready') {
         schtasks /Delete /tn PSWindowsUpdate /F
     }
 }
 
+# To avoid unexpected $LASTEXITCODE other than 0 (zero)
+exit 0
