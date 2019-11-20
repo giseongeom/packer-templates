aadfbc7352f0cb1a448d37d90d7dd2258154e5a6
GiSeong Eom <giseong.eom@krafton.com>
2019-11-20 23:04:52 +0900

    Update VMware Tools (11.0.1)

--- vmware/scripts/install-guest-tools-on-win10.ps1
+++ vmware/scripts/install-guest-tools-on-win10.ps1
@@ -2,22 +2,22 @@
 
 if ($ENV:PACKER_BUILDER_TYPE -eq "vmware-iso") {
     # VMware tools
-    $iso_url  = 'https://packages.vmware.com/tools/releases/10.3.10/windows/VMware-tools-windows-10.3.10-12406962.iso'
+    $iso_url  = 'https://packages.vmware.com/tools/releases/11.0.1/windows/VMware-tools-windows-11.0.1-14773994.iso'
     $iso_path = "C:\Windows\Temp\windows.iso"
-    (New-Object Net.WebClient).DownloadFile($iso_url, $iso_path) 
+    (New-Object Net.WebClient).DownloadFile($iso_url, $iso_path)
 } else {
     # VirtualBox Additions
     $iso_url  = 'https://download.virtualbox.org/virtualbox/6.0.6/VBoxGuestAdditions_6.0.6.iso'
     $iso_path = "C:\Windows\Temp\windows.iso"
-    (New-Object Net.WebClient).DownloadFile($iso_url, $iso_path) 
+    (New-Object Net.WebClient).DownloadFile($iso_url, $iso_path)
 }
 
 If (Test-Path $iso_path) {
     Write-Output "Mounting disk image at $iso_path"
     Mount-DiskImage -ImagePath $iso_path
     $iso_mounted_drv = ((Get-DiskImage -ImagePath $iso_path | Get-Volume).Driveletter  + ':')
-    robocopy $iso_mounted_drv c:\windows\temp\vmtools /mir /timfix /np /nfl /ndl /r:0 
-    # https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/ 
+    robocopy $iso_mounted_drv c:\windows\temp\vmtools /mir /timfix /np /nfl /ndl /r:0
+    # https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/
     Start-Sleep 5
 }
 
@@ -33,7 +33,7 @@ function virtualbox {
         #$certdir = ((Get-DiskImage -ImagePath $iso_path | Get-Volume).Driveletter + ':\cert\')
         $certdir = 'c:\windows\temp\vmtools\cert\'
         $VBoxCertUtil = ($certdir + 'VBoxCertUtil.exe')
-        
+
         if (Test-Path ($VBoxCertUtil)) {
             Write-Output "Using newer (4.4 and above) certificate import method"
         	Get-ChildItem $certdir *.cer | ForEach-Object { & $VBoxCertUtil add-trusted-publisher $_.FullName --root $_.FullName}
@@ -46,8 +46,8 @@ function virtualbox {
     } else {
         $vboxaddondrv = gcim win32_logicaldisk | Where-Object { $_.volumename -like "*vbox*" }
         if (($vboxaddondrv | Measure-Object).Count -gt 0) {
-            robocopy $vboxaddondrv.DeviceID c:\windows\temp\vmtools /mir /timfix /np /nfl /ndl /r:0 
-            # https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/ 
+            robocopy $vboxaddondrv.DeviceID c:\windows\temp\vmtools /mir /timfix /np /nfl /ndl /r:0
+            # https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/
             Start-Sleep 10
 
             $certdir = 'c:\windows\temp\vmtools\cert\'
@@ -81,5 +81,5 @@ If (Test-Path $iso_path) {
 }
 
 # Due to unexpected LASTEXITCODE of "robocopy"
-# https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/ 
+# https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/
 exit 0
